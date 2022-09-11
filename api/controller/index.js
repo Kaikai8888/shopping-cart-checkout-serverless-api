876c1237c49503e14abc856f89dad9ed4b132ca4
Kaikai8888 <kelly.wu@aimazing.co>
2022-09-11 14:25:00 +0800

    feat: add controllers

---
+++ api/controller/index.js
@@ -0,0 +1,11 @@
+const authenticate = require('./authenticate')
+const order = require('./order')
+const product = require('./product')
+const user = require('./user')
+
+module.exports = {
+  authenticate,
+  order,
+  product,
+  user
+}
